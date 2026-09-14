971d6812d06d81ed03eb32c1c0b62502c0d30240
Yongkang Yang <ykyang.research@gmail.com>
2026-09-13 18:45:01 -0700

    chore: bump TrackTab app version

--- build_app.sh
+++ build_app.sh
@@ -31,9 +31,9 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.1.0</string>
+    <string>0.2.0</string>
     <key>CFBundleVersion</key>
-    <string>1</string>
+    <string>2</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>LSUIElement</key>
